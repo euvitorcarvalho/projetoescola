4ffd8b68c8283d20fef4f9d0cac4043e26f1a9f9
Guilherme Almeida Nascimento Santos <allmeida.guigui@gmail.com>
2026-09-08 18:49:06 +0000

    Refactor: mudanças na leitura e atribuição dos dados. Feat: implementação de validações. Feat: função de exibição de dados do aluno

--- aluno.c
+++ aluno.c
@@ -1,43 +1,121 @@
 #include "aluno.h"
+#include "validacoes.h"
 #include <stdio.h>
 #include <string.h>
 
 int main(){
     Aluno aluno;
     cadastrarAluno(&aluno);
-    printf("Cadastro concluido!");
+    printf("\nCadastro concluido!\n");
+    imprimirDadosAluno(aluno);
 }
 
-void cadastrarAluno(Aluno *aluno){ // recebe o endereço do aluno para alterar
+void imprimirDadosAluno(Aluno a){
+    printf("\n-----Dados do Aluno-----\n");
+    printf("Nome: %s\n",a.nome);
+
+    if (a.sexo == 'M' || a.sexo == 'm')
+        printf("Sexo: Masculino\n");
+    else if(a.sexo == 'F' || a.sexo == 'f')
+        printf("Sexo: Feminino\n");
+    else if(a.sexo == 'O' || a.sexo == 'o')
+        printf("Sexo: Outro\n");
     
+    printf("Data de nascimento: %02d/%02d/%04d\n",
+        a.dataNascimento.dia,
+        a.dataNascimento.mes,
+        a.dataNascimento.ano);
+
+    printf("CPF: %s\n",a.CPF); //exibir cpf formatado em breve
+
+    printf("Matricula: %d\n",a.matricula);
+
+}
+
+void cadastrarAluno(Aluno *pAluno){ // recebe o endereço do aluno para alterar
+
+    // cópia das variaveis do aluno, para leiura e validação antes de atribuir ao aluno
+    //int matricula;
+    char nome[100];
+    char sexo[3];
+    char cpf[12];
+    TipoData data;
+
+    int invalido; // verificador de validade
+
+    //LER NOME
     printf("Digite seu nome: ");
-    lerNome(aluno->nome, sizeof(aluno->nome));
+    ler_str(nome, sizeof(nome)); // nome precisa de validação?
+
+    // LER SEXO
+    do{
+        printf("\n--------Sexo-------\n");
+        printf("M - Masculino\n ");
+        printf("F - Feminino\n ");
+        printf("O - Outro\n ");
+
+        printf("\nSelecione seu sexo: ");
+
+        ler_str(sexo, sizeof(sexo));  //ler a opção selecionada + \n + \0
+        invalido = validar_sexo(sexo); // validação ainda em desenvolvimento. verificar se uma das opções foi selecionada, case insensitive
+
+        switch(invalido){
+            case 1 : printf("Selecione uma opção valida."); break;
+            default : break;
+        }
+
+    }while(invalido);
+
+    //LER CPF
+    do{
+        printf("Digite seu CPF:");
+        ler_str(cpf, sizeof(cpf));
+        invalido = validar_cpf(cpf);
+
+        switch(invalido){
+            case 0 : break;
+            case 1 : printf("Erro 1 - Cpf digitado não contem 11 dígitos.\n"); break;
+            case 2 : printf("Erro 2 - Digite apenas numeros, sem pontos ou traços.\n"); break;
+            case 3 : printf("Erro 3 - Cpf digitado possui todos os caracteres iguais.\n"); break;
+            case 4 : printf("Erro 4 - Erro no 1º dígito verificador.\n"); break;
+            case 5 : printf("Erro 5 - Erro no 2º dígito verificador\n."); break;
+            default: printf("Erro desconhecido - falha na leitura.\n"); break;
+        }
+
+    }while (invalido);
+
+    // LER DATA DE NASCIMENTO
+    do{
+        printf("Digite sua data de nascimento (ddmmaaaa):");
+        lerData(&data);
+
+        invalido = validar_data(data); // validação ainda em desenvolvimento. Verificar se dia está entre 1 e 31, se mês está entre 1 e 12. e caso seja mes fevereiro, verificar se dia está entre 1 e 28 ou 29 caso seja ano bissexto.
+
+        switch (invalido){
+            case 1 : printf("Digite uma data valida."); break;
+            default : break;
+        }
+    }while(invalido); // repete a leitura enquanto for invalido
+
+    //ATRIBUIÇÃO - se os dados forem validos, atribuir ao aluno
     
-    printf("Digite sua data de nascimento (ddmmaaaa):");
-    lerData(&aluno->dataNascimento);
-    // validar data depois
-
-    printf("Selecione seu sexo:\n ");
-    printf("M - Masculino\n ");
-    printf("F - Feminino\n ");
-
-    lerSexo(&aluno->sexo);
-
-    printf("Digite seu CPF:");
-    lerCPF(aluno->CPF, sizeof(aluno->CPF));
-    // validar cpf depois
+    strcpy(pAluno->nome, nome);
+    strcpy(pAluno->CPF, cpf);//                                                   0    1    2    
+    pAluno->sexo = sexo[0]; // passa primeira letra lida da string de sexo. ex: | M | \n | \0 |
+    pAluno->dataNascimento = data;
+    // falta gerar matricula;
     
 }
 
 //DEFINIÇÃO DAS FUNÇÕES DE LEIURA
 
-void lerNome(char nome[], int tamanho){ // recebe a string e o tamanho dela
+void ler_str(char str[], int tamanho){ // recebe uma string e o tamanho dela
 
-    // ler o nome com o fgets
-    fgets(nome, tamanho, stdin); 
+    // ler a string com o fgets
+    fgets(str, tamanho, stdin); 
 
     // remove o \n que o fgets considera ao apertar enter
-    nome[strcspn(nome, "\n")] = '\0';
+    str[strcspn(str, "\n")] = '\0';
 }
 
 
@@ -49,25 +127,6 @@ void lerData(TipoData *pData){ // recebe o endereço da variavel do tipo Data
 
     // chama função para separar em dia, mes e ano
     *pData = DiaMesAno(num);
-}
-
-void lerSexo(char *pSexo){ // recebe o endereço de onde salvar o char
-   
-    // ler o sexo com o scanf
-   scanf(" %c",pSexo);
-
-   // corrige erro de pular a leitura do CPF, algum problema de buffer, eu acho
-   getchar(); 
-}
-
-void lerCPF(char CPF[], int tamanho){ // recebe a string e o tamanho dela
-    
-    // ler o array com o fgets
-    fgets(CPF, tamanho, stdin);
-
-    // remove o \n que o fgets considera ao apertar enter
-    CPF[strcspn(CPF, "\n")] = '\0';
-
 }
 
 TipoData DiaMesAno(int num){ // recebe um numero inteiro no formato ddmmaaaa e retorna uma data separada em dia, mes e ano
--- aluno.h
+++ aluno.h
@@ -13,20 +13,19 @@ typedef struct
     int matricula; // número de matrícula do aluno
     char nome[100]; 
     char sexo; // 
-    TipoData dataNascimento; // data com dia, mês e ano separados
     char CPF[12]; // formato: XXXXXXXXXXX\0
+    TipoData dataNascimento; // data com dia, mês e ano separados
+
 } Aluno;
+
+void imprimirDadosAluno(Aluno a); // exibe informações do aluno
 
 void cadastrarAluno(Aluno *aluno); // cadastra o aluno
 
 TipoData DiaMesAno(int num); // ler um inteiro no formato ddmmaaaa e separa os numeros em dia, mes, ano
 
-void lerNome(char nome[], int tamanho); // ler o nome do aluno
+void ler_str(char nome[], int tamanho); // ler uma string com o fgets e tira o \n que ele considera
 
-void lerData(TipoData *pData); // recebe o ponteiro da data e lê a data de nascimento
-
-void lerSexo(char *pSexo); // recebe o ponteiro do char do sexo e lê. considerar 'M' para masculino, 'F' para feminino; case insensitive
-
-void lerCPF(char cpf[], int tamanho); // ler char de cpf no formato: xxxxxxxxxxx
+void lerData(TipoData *pData); // recebe o ponteiro da data e lê a data 
 
 #endif
